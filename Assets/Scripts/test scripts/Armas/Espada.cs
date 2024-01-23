7f2ad3f31d0d532ef34cc0ceb63ef969e8569cf8
David <david.pascal.7e6@itb.cat>
2024-01-23 13:29:26 +0100

    Arreglillos y Perosnaje Func con Anim Y algo maas no me acuerdo

--- Assets/Scripts/test scripts/Armas/Espada.cs
+++ Assets/Scripts/test scripts/Armas/Espada.cs
@@ -32,14 +32,6 @@ public class GolpeDeEspada : MonoBehaviour
         // Detectar enemigos dentro del rango del golpe
         Collider2D[] enemigosGolpeados = Physics2D.OverlapCircleAll(posicionMouse, rangoGolpe, capaEnemigos);
 
-        // Aplicar daño a los enemigos dentro del rango
-        foreach (Collider2D enemigo in enemigosGolpeados)
-        {
-            // Aquí puedes agregar lógica adicional si es necesario (por ejemplo, reproducir efectos de sonido, animaciones, etc.)
-            // Aplicar daño al enemigo
-            enemigo.GetComponent<VidaEnemigo>().RecibirDanio(danio);
-        }
-
         // Actualizar el tiempo del último golpe
         tiempoUltimoGolpe = Time.time;
     }
--- Assets/Scripts/test scripts/Enemy/VEnem.cs
+++ Assets/Scripts/test scripts/Enemy/VEnem.cs
@@ -2,31 +2,11 @@ using UnityEngine;
 
 public class VidaEnemigo : MonoBehaviour
 {
-    public int vidaInicial = 1; // Vida inicial del enemigo
-    private int vidaActual; // Vida actual del enemigo
-
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        vidaActual = vidaInicial;
-    }
-
-    // Método para recibir daño
-    public void RecibirDanio(int cantidad)
-    {
-        vidaActual -= cantidad;
-
-        // Verificar si la vida llegó a cero
-        if (vidaActual <= 0)
+        if (collision.gameObject.CompareTag("Bullet"))
         {
-            // Llamar a un método de destrucción o simplemente destruir el objeto
-            DestruirEnemigo();
+            Destroy(gameObject); // Se destruirá inmediatamente al tocar la bala
         }
-    }
-
-    // Método para destruir el enemigo
-    void DestruirEnemigo()
-    {
-        // Puedes agregar aquí lógica adicional antes de destruir el objeto (por ejemplo, efectos visuales)
-        Destroy(gameObject);
     }
 }
