a310670b9f86f2be67cf31b35f03eb0b4f900b29
Oriol Casals Silles <oriolcs04@gmail.com>
2024-02-04 23:46:47 +0100

    musica i botiga arreglada

--- Assets/Scripts/ScriptsTienda/Tendero.cs
+++ Assets/Scripts/ScriptsTienda/Tendero.cs
@@ -5,9 +5,31 @@ using UnityEngine;
 public class Tendero : MonoBehaviour
 {
     [SerializeField] private GameObject tienda;
+    [SerializeField] private GameObject interact;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        tienda.SetActive(true);
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            interact.SetActive(true);
+        }
     }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        interact.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (tienda.activeInHierarchy == true && Input.GetKeyDown(KeyCode.E))
+        {
+            tienda.SetActive(false);
+        }
+        else if (interact.activeInHierarchy == true && Input.GetKeyDown(KeyCode.E))
+        {
+            tienda.SetActive(true);
+        }
+    }
+
 }
